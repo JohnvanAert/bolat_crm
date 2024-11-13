ca2310471f3dff3fb43ad0b04f2f1e09773a9629
John van Aert <johnvanaert89@gmail.com>
2024-11-13 14:07:45 +0500

    fixed resizing

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ def create_navigation(root, show_main_page, show_products_page, show_gui_page, s
 def main():
     root = tk.Tk()
     root.title("CRM Navigation Panel")
+    root.geometry("1024x768")  # Устанавливаем начальный размер окна
+    root.minsize(800, 600)     # Устанавливаем минимальный размер окна
+    root.maxsize(1920, 1080)   # Устанавливаем максимальный размер окна
+
+    def resize_handler(event):
+        # Обработка изменения размера, если необходимо
+        #print(f"New size: {event.width}x{event.height}")
+
+        root.bind("<Configure>", resize_handler)
+
 
     def show_main_page():
         frame_products.pack_forget()
@@ -24,34 +34,35 @@ def main():
         frame_cabin.pack_forget()
         frame_expenses.pack_forget()
         welcome_label.pack()
-
+        root.update_idletasks()
     def show_products_page():
         welcome_label.pack_forget()
         frame_gui.pack_forget()
         frame_cabin.pack_forget()
         frame_expenses.pack_forget()
         frame_products.pack()
-
+        root.update_idletasks()
     def show_gui_page():
         welcome_label.pack_forget()
         frame_products.pack_forget()
         frame_cabin.pack_forget()
         frame_expenses.pack_forget()
         frame_gui.pack()
-        
+        root.update_idletasks()
     def show_cabin_page():
         welcome_label.pack_forget()
         frame_products.pack_forget()
         frame_gui.pack_forget()
         frame_expenses.pack_forget()
         frame_cabin.pack()
-
+        root.update_idletasks()
     def show_expenses_page():
         welcome_label.pack_forget()
         frame_products.pack_forget()
         frame_gui.pack_forget()
         frame_cabin.pack_forget()
         frame_expenses.pack()
+        root.update_idletasks()
 
     welcome_label = tk.Label(root, text="Добро пожаловать на главную страницу!")
     welcome_label.pack()
